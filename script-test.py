a2c0fa2fb6b87728018365da9400716d798e24b1
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2025-12-31 10:28:03 +0530

    Update script-test.py

--- script-test.py
+++ script-test.py
@@ -216,6 +216,6 @@ else:
 # Launch WebUI FOR NORMAL FORGE
 #LAUNCH_CMD = f"python launch.py {share} --gpu-device-id 1 --xformers --cuda-stream --always-gpu --pin-shared-memory --api --listen --enable-insecure-extension-access {auth} --disable-console-progressbars --no-hashing --precision autocast --upcast-sampling"
 # FOR RE-FROGE
-LAUNCH_CMD = f"python launch.py {share} --xformers --api --listen --enable-insecure-extension-access {auth} --disable-console-progressbars --no-hashing"
+LAUNCH_CMD = f"python launch.py {share} --api --listen --enable-insecure-extension-access {auth} --disable-console-progressbars --no-hashing"
 print(f"Starting WebUI with command:\n{LAUNCH_CMD}")
 os.system(LAUNCH_CMD)
