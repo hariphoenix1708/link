51a0adbbf49ecafe5c4bbe689066902c777aa352
hariphoenix1708 <138122182+hariphoenix1708@users.noreply.github.com>
2026-04-11 11:26:34 +0530

    Update script-test-final.py

--- script-test-final.py
+++ script-test-final.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 import time
@@ -19,7 +18,7 @@ except ImportError:
     from pyngrok import ngrok, conf
 
 # Kaggle secret (preferred) with environment fallback
-CIVITAI_TOKEN = ""
+CIVITAI_TOKEN = "52c41327b3f7485898faf9e92a29e008"
 try:
     from kaggle_secrets import UserSecretsClient
     secret_label = "CIVITAI_TOKEN"
